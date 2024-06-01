0e5d2829e917a2523e29348101ddb270cbfbfe7c
OussamaGHODBANE <96655806+OussamaGHODBANE@users.noreply.github.com>
2024-06-01 16:33:19 +0100

    Update eureka_config.py

--- api/eureka_config.py
+++ api/eureka_config.py
@@ -15,12 +15,12 @@ async def start_eureka_client():
 
     # Get the local machine's IP address
     ip_address = get_ip_address()
-    
+    print(ip_address)
     eureka_client = EurekaClient(
         eureka_server="https://ntic-discovery-server2.onrender.com/eureka/",
         app_name="price_prediction",
         instance_port=8000,  # The port your Django app is running on
-        # instance_ip=ip_address,  # The IP address your Django app is running on
+        instance_ip=ip_address,  # The IP address your Django app is running on
         renewal_interval_in_secs=30,  # Heartbeat interval
         duration_in_secs=90  # Expiry duration
     )
